94caeb1d6fb4e0d0ab14d3684d700b3a79a52aa3
Jordan Nelson <jnelsontn@gmail.com>
2017-03-09 01:08:21 -0600

    large commit. all basic functionality in.

--- dist/app.js
+++ dist/app.js
@@ -172,16 +172,91 @@ module.exports = GuildHall;
 },{}],2:[function(require,module,exports){
 "use strict";
 
+ var GuildHall = require("./classes.js"),
+     Combatants = require("./player.js"),
+     Tools = require("./weapons.js");
+
+let CreatePlayer = {};
+
+CreatePlayer.createPlayer = (setName, setClass, setWeapon) => {
+
+      console.log('createPlayer init: Parameters passed: ', setName, setClass, setWeapon);
+
+      let newPlayer = new Combatants.Human();
+          newPlayer.playerName = setName;
+
+      // we could attach an images for a class, weapon, species, etc.
+
+      if (setClass === 'Warrior') {
+        newPlayer.class = new GuildHall.Warrior();
+      } else if (setClass === 'Valkyrie') {
+        newPlayer.class = new GuildHall.Valkyrie();
+      } else if (setClass === 'Berserker') {
+        newPlayer.class = new GuildHall.Berserker();
+      } else if (setClass === 'Monk') {
+        newPlayer.class = new GuildHall.Monk();
+      } else if (setClass === 'Wizard') {
+        newPlayer.class = new GuildHall.Wizard();
+      } else if (setClass === 'Sorcerer') {
+        newPlayer.class = new GuildHall.Sorcerer();
+      } else if (setClass === 'Conjurer') {
+        newPlayer.class = new GuildHall.Conjurer();
+      } else if (setClass === 'Thief') {
+        newPlayer.class = new GuildHall.Thief();
+      } else if (setClass === 'Ninja') {
+        newPlayer.class = new GuildHall.Ninja();
+      } else if (setClass === 'Assassin') {
+        newPlayer.class = new GuildHall.Assassin();
+      } else {
+        console.log('createPlayer: No Class Set - Should not happen');
+        newPlayer.class = 'noClass';
+      }
+      // console.log('startGame: Player has Class Attached');
+
+      if (setWeapon === 'Dagger' ) {
+        newPlayer.setWeapon(new Tools.Dagger());
+      } else if (setWeapon === 'BroadSword') {
+        newPlayer.setWeapon(new Tools.BroadSword());
+      } else if (setWeapon === 'WarAxe') {
+        newPlayer.setWeapon(new Tools.WarAxe());
+      } else {
+        console.log('createPlayer: No Weapon Set - Should not happen');
+        newPlayer.weapon = 'noWeapon';
+      }
+      // console.log('createPlayer: Player has Weapon Attached');
+
+      console.log('createPlayer: Player Created');
+
+      return newPlayer;
+};
+
+module.exports = CreatePlayer;
+},{"./classes.js":1,"./player.js":5,"./weapons.js":8}],3:[function(require,module,exports){
+"use strict";
+
 
 console.log("enemies.js linked");
 
  var GuildHall = require("./classes.js"),
-     Combatants = require("./player.js");
+     Combatants = require("./player.js"),
+     Tools = require("./weapons.js");
 
 let Enemies = {};
 
+Enemies.createEnemy = () => {
+
+  var orc = new Enemies.Orc();
+  orc.generateClass();
+  orc.setWeapon(new Tools.BroadSword());
+
+  console.log('createEnemy: Enemy Created');
+
+  return orc;
+};
+
 Enemies.Orc = function() {
 
+  this.playerName = "Orcy McOrc Face";
   this.health = this.health + 20;
   this.species = "Orc";
   this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
@@ -202,24 +277,134 @@ Enemies.Orc = function() {
 Enemies.Orc.prototype = new Combatants.Monster();
 
 module.exports = Enemies;
-},{"./classes.js":1,"./player.js":4}],3:[function(require,module,exports){
+},{"./classes.js":1,"./player.js":5,"./weapons.js":8}],4:[function(require,module,exports){
 "use strict";
 
-console.log("app.js linked");
+console.log("main.js linked");
 
 let Tools = require("./weapons.js"),
-  Combatants = require("./player.js"),
-  SpellBook = require("./spells.js"),
-  GuildHall = require('./classes.js'),
-  Enemies = require('./enemies.js');
+    Combatants = require("./player.js"),
+    SpellBook = require("./spells.js"),
+    GuildHall = require('./classes.js'),
+    Enemies = require('./enemies.js'),
+    CreatePlayer = require('./createplayer.js'),
+    StartCombat = require('./startcombat.js');
+
+let classChoosen, 
+    weaponChoosen,
+    createdPlayer,
+    createdEnemy;
+
+$(document).ready(function() {
+  $("#player-setup").show();
+});
+
+$(".card__link").click( (e) => {
+  handleSetup(e);
+});
+
+function handleSetup (e) {
+    var nextCard = $(e.currentTarget).attr("next");
+    var playerName = $("#player-name").val();
+    var moveAlong = false;
+
+    // start loop: if nextCard is EQUAL to an <a next="card--X"> go to next 
+    switch (nextCard) {
+
+      case "card--class":
+        moveAlong = ($("#player-name").val() !== "");
+
+        $('.class__link').click(function() {
+          classChoosen = $(this).attr('id');
+          console.log('handleSetup: card--card: You choose the class:', classChoosen);
+        });
+
+        break;
+
+      case "card--weapon":
+        moveAlong = (classChoosen !== undefined);
+
+        $('.weapon__link').click(function() {
+          weaponChoosen = $(this).attr('id');
+          console.log('handleSetup: card--weapon: You choose the weapon: ', weaponChoosen);
+        });
+
+        break;
+
+      case "card--battleground":
+        moveAlong = (weaponChoosen !== undefined);
+
+        createdPlayer = CreatePlayer.createPlayer(playerName, classChoosen, weaponChoosen);
+
+          let playerStats =
+            '<br />' + 'name: ' + createdPlayer.playerName +
+            '<br />' + 'class: ' + createdPlayer.class.name +
+            '<br />' + 'weapon: ' + createdPlayer.weapon.name +
+            '<br />' + 'species: ' + createdPlayer.species +
+            '<br />' + 'health: ' + createdPlayer.health +
+            '<br />' + 'intelligence: ' + createdPlayer.intelligence;
+            $('.playerStats').append(playerStats);
+
+        createdEnemy = Enemies.createEnemy();
+
+          let enemyStats =
+            '<br />' + 'name: ' + createdEnemy.playerName +
+            '<br />' + 'class: ' + createdEnemy.class.name +
+            '<br />' + 'weapon: ' + createdEnemy.weapon.name +
+            '<br />' + 'species: ' + createdEnemy.species +
+            '<br />' + 'health: ' + createdEnemy.health +
+            '<br />' + 'intelligence: ' + createdEnemy.intelligence;
+            $('.enemyStats').append(enemyStats);
+
+        break;
+
+      case "card--runbattle":
+        moveAlong = true;
+
+        StartCombat.playerVersusEnemy(createdPlayer, createdEnemy);
+
+        $('#playagain').click( () => {
+          console.log('not yet');
+          /* we need to create a new instance of obj or values will be the same... below code doesn't work
+          createdPlayer = CreatePlayer.createPlayer(playerName, classChoosen, weaponChoosen);
+          createdEnemy = Enemies.createEnemy();
+          StartCombat.playerVersusEnemy(createdPlayer, createdEnemy);
+          */
+        });
+
+        break;
+
+    }
+
+    /* When True: Set Class 'Card' to Hide (hides everything)
+       Then Specifically Set the Class with the a.next to Show. */
+    if (moveAlong) { 
+      $(".card").hide();
+      $("." + nextCard).show();
+    } else {
+      console.log('Set a Name or Pick a Class or Pick a Weapon');
+    }
+
+    $(".card__back").click(function() {
+      var previousCard = $(this).attr("previous");
+      $(".card").hide();
+      $("." + previousCard).show();
+    });
+
+}
+
+function showPlayerStats() {
+  var outputPlayerStats = document.getElementsByClassName("playerStats");
+}
+
+function showEnemyStats() {
+  var outputEnemyStats = document.getElementsByClassName("enemyStats");
+}
 
 /*
   Test code to generate a human player and an orc player
  */
-// var orc = new Enemies.Orc();
-// orc.generateClass();
-// orc.setWeapon(new Tools.BroadSword());
-// console.log(orc.toString());
+
 
 // var warrior = new Combatants.Human();
 // warrior.setWeapon(new Tools.WarAxe());
@@ -230,173 +415,9 @@ let Tools = require("./weapons.js"),
  */
 // var spell = new SpellBook.Sphere();
 // console.log("spell: ", spell.toString());
+// 
 
-var playerName;
-var classChoosen;
-var weaponChoosen;
-
-$(document).ready(function() {
-  /*
-    Show the initial view that accepts player name
-   */
-  $("#player-setup").show();
-
-  /*
-    When any button with card__link class is clicked,
-    move on to the next view.
-   */
-  $(".card__link").click(function(e) {
-    var nextCard = $(this).attr("next");
-
-    var moveAlong = false;
-  
-
-    // we should not allow a blank name here as initial view is asking for name
-    playerName = $("#player-name").val();
-    // console.log('Hello ', playerName);
-
-    switch (nextCard) { // next card is set to next attribute on href
-      case "card--class": // first ahref next -- this is class of ahref and SECTION
-                          // this shows which section we're on
-
-        moveAlong = ($("#player-name").val() !== "");
-        console.log(moveAlong);
-
-        $('.class__link').click(function(e) {
-          // Set the class here
-          classChoosen = $(this).attr('id');
-          classChoosen = classChoosen.toString();
-          console.log('You choose the class: ', classChoosen);
-        });
-
-        break;
-
-      case "card--weapon":
-        // now pick a weapon
-        console.log('You are a ', classChoosen);
-        moveAlong = ($("#player-name").val() !== "");
-
-        $('.weapon__link').click(function(e) {
-          // Set the weapon here
-          weaponChoosen = $(this).attr('id');
-          weaponChoosen = weaponChoosen.toString();
-
-          console.log('You choose the weapon: ', weaponChoosen);
-        });
-
-        break;
-
-      case "card--battleground":
-
-        console.log(weaponChoosen, classChoosen);
-        moveAlong = ($("#player-name").val() !== "");
-        console.log('we should start a battle here');
-
-        startGame();
-
-      break;
-
-    }
-
-    function startGame () {
-/*     var playerName; var classChoosen; var weaponChoosen;
-      we need to use this variable to set what we have
-  Classes:
-    Warrior, Valkyrie, Berserker, Monk, Wizard, Sorcerer, Conjurer 
-    Thief, Ninja, Assassin
-  Weapons:
-    Dagger, BroadSword, WarAxe
-    */
-
-      /* this works but we should set the props equal to the
-        values we set above */
-
-      console.log('Class Picked Is: ', classChoosen);
-
-      var newPlayer;
-      // should create default player class here, do we start with class?
-
-/*
- 
-      if (classChoosen === 'Warrior') {
-        newPlayer =
-        newPlayer.class =
-      } else if  (classChoosen === 'Valkyrie') {
-        newPlayer = 
-        newPlayer.class =
-      } else if  (classChoosen === 'Berserker') {
-        newPlayer = 
-        newPlayer.class =    
-      } else if  (classChoosen === 'Monk') {
-        newPlayer = 
-        newPlayer.class = 
-      } else if  (classChoosen === 'Wizard') {
-        newPlayer = 
-        newPlayer.class = 
-      } else if  (classChoosen === 'Sorcerer') {
-        newPlayer = 
-        newPlayer.class = 
-      } else if  (classChoosen === 'Conjurer') {
-        newPlayer = 
-        newPlayer.class = 
-      } else if  (classChoosen === 'Thief') {
-        newPlayer = 
-        newPlayer.class =
-      } else if  (classChoosen === 'Ninja') {
-        newPlayer = 
-        newPlayer.class =
-      } else if  (classChoosen === 'Assassin') {
-        newPlayer = 
-        newPlayer.class =
-      } else {
-        console.log('what is going on? ');
-      }
-*/
-      var playerWeapon = 'anything at the moment';
-
-      var warrior = new Combatants.Human();
-      warrior.playerName = playerName;
-
-      // warrior.setWeapon(new WarAxe());
-      // warrior.generateClass();  // This will be used for "Surprise me" option
-      warrior.weapon = playerWeapon;
-      warrior.class = new GuildHall.Warrior();
-
-      console.log(warrior.toString());
-      console.log(warrior);
-
-    }
-
-    if (moveAlong) {
-      console.log('where are we: ', $(this));
-      $(".card").hide();
-      $("." + nextCard).show();
-    }
-  });
-
-  /*
-    When the back button clicked, move back a view
-   */
-  $(".card__back").click(function(e) {
-    var previousCard = $(this).attr("previous");
-    $(".card").hide();
-    $("." + previousCard).show();
-  });
-
-
-  function showPlayerStats() {
-    var outputPlayerStats = document.getElementsByClassName("playerStats");
-  }
-
-  function showEnemyStats() {
-    var outputEnemyStats = document.getElementsByClassName("enemyStats");
-  }
-
-
-});
-
-
-},{"./classes.js":1,"./enemies.js":2,"./player.js":4,"./spells.js":5,"./weapons.js":6}],4:[function(require,module,exports){
+},{"./classes.js":1,"./createplayer.js":2,"./enemies.js":3,"./player.js":5,"./spells.js":6,"./startcombat.js":7,"./weapons.js":8}],5:[function(require,module,exports){
 "use strict";
 /*
   TODO: Modularize this code with IIFE or Browserify
@@ -494,7 +515,7 @@ Combatants.Monster.prototype = new Combatants.Player();
 module.exports = Combatants;
 
 
-},{"./classes.js":1}],5:[function(require,module,exports){
+},{"./classes.js":1}],6:[function(require,module,exports){
 "use strict";
 /*
   TODO: Modularize this code with IIFE or Browserify
@@ -531,7 +552,60 @@ SpellBook.Sphere = function() {
 SpellBook.Sphere.prototype = new SpellBook.Spell();
 
 module.exports = SpellBook;
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
+'use strict';
+
+var StartCombat = {};
+
+StartCombat.playerVersusEnemy = (player, enemy) => {
+	$('.combatContainer').empty();
+
+	console.log('player obj: ', player);
+	console.log('enemy obj: ', enemy);
+
+	console.log(player.toString() );
+	console.log(enemy.toString() );
+
+	if ( player.health > 0 || enemy.health > 0 ) {
+		let num = 1;
+
+		while (true) {
+			console.log('Round ', num);
+
+			player.health = player.health - enemy.weapon.damage;
+			console.log(enemy.playerName + ' attacks ' + player.playerName + ' for ' + enemy.weapon.damage + 'hp. ' + player.playerName + `'s` + ' health is now ' + player.health);
+
+			if ( player.health <= 0 ) {
+				console.log(player.playerName + ' has lost.');
+
+				let playerLost = `${player.playerName} has lost.` + '<br /><br />' + '<b>' + `${enemy.playerName} has won.` + '</b>';
+				$('.combatContainer').append(playerLost);
+
+				break;
+			}
+
+			enemy.health = enemy.health - player.weapon.damage;
+			console.log(player.playerName + ' attacks ' + enemy.playerName + ' for ' + player.weapon.damage + 'hp. ' + enemy.playerName + `'s` + ' health is now ' + enemy.health);
+
+			if ( enemy.health <= 0 ) {
+				console.log(enemy.playerName + ' has lost.');
+				
+				let enemyLost = `${enemy.playerName} has lost.` + '<br /><br />' + '<b>' + `${player.playerName} has won.` + '</b>';
+				$('.combatContainer').append(enemyLost);
+
+				break;
+			}
+
+			num++;
+
+		} // end loop
+	} // end if
+	
+};
+
+module.exports = StartCombat;
+
+},{}],8:[function(require,module,exports){
 "use strict";
 
 
@@ -572,4 +646,4 @@ Tools.WarAxe = function() {
 Tools.WarAxe.prototype = new Tools.Weapon();
 
 module.exports = Tools;
-},{}]},{},[1,2,3,4,5,6]);
+},{}]},{},[1,2,3,4,5,6,7,8]);
